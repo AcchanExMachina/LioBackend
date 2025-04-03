20a3e492c570742f98be846086392b5d81184e75
Samir Kanoun <kanounsamir@gmail.com>
2025-04-03 22:45:51 +0200

    serialized

--- src/Controller/ArtworkController.php
+++ src/Controller/ArtworkController.php
@@ -1,19 +1,22 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Artwork;
 use App\Service\ArtworkService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/artwork')]
 class ArtworkController extends AbstractController
 {
     #[Route('/list')]
-    public function list(ArtworkService $artworkService): Response
+    public function list(SerializerInterface $serializer, ArtworkService $artworkService): Response
     {
-        var_dump($artworkService->getAll());
-
-        return new Response();
+        $jsonContent = $serializer->serialize($artworkService->getAll(), 'json');
+        
+        return JsonResponse::fromJsonString($jsonContent);
     }
 }
